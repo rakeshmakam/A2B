4bea8a3a566f90a9abaa5fd5829585bfa5c3746e
harsh-rayzada <harsh.rayzada@gmail.com>
2015-11-02 17:31:06 +0530

    added API for merchant and user existance

--- Payment_Gateway/api/controllers/MerchantController.js
+++ Payment_Gateway/api/controllers/MerchantController.js
@@ -5,6 +5,7 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 var Client = require('node-rest-client').Client;
+var Aes = require('crypto-libraries/aes-ctr');
 
 module.exports = {
 	get: function(req, res){
@@ -17,34 +18,54 @@ module.exports = {
 		});
 	},
 
-	merchantUserLogin: function(req, res){
-		var client = new Client();
+	autoLogin: function(req, res){
+		var payloadFlag = false;
+		var cipherText = req.body.payload;
+		var plainText = Aes.decrypt(cipherText, 'rakesh', 256);
+		if(cipherText.email != req.body.email){
+			payloadFlag = true;
+		}else if(cipherText.phoneNumber != req.body.phoneNumber){
+			payloadFlag = true;
+		}else if(cipherText.billingAddress.street != req.body.street){
+			payloadFlag = true;
+		}else if(cipherText.billingAddress.city != req.body.city){
+			payloadFlag = true;
+		}else if(cipherText.billingAddress.postalCode != req.body.postalCode){
+			payloadFlag = true;
+		}else if(cipherText.billingAddress != req.body.state){
+			payloadFlag = true;
+		}else if(payloadFlag == true){
+			res.status(401).json({error:'Data unauthorized!'});
+		}else{
+			var client = new Client();
 
-		var args = {
-			data: {
-				email: req.body.email,
-				phoneNumber: req.body.phoneNumber,
-				billingAddress: {
-					street: req.body.street,
-					city: req.body.city,
-					postalCode: req.body.postalCode,
-					state: req.body.state
+			var args = {
+				data: {
+					email: req.body.email,
+					phoneNumber: req.body.phoneNumber,
+					payload: req.body.payload,
+					billingAddress: {
+						street: req.body.street,
+						city: req.body.city,
+						postalCode: req.body.postalCode,
+						state: req.body.state
+					}
+		    	},
+		    	headers:{
+		    		"Authorization" : req.headers.authorization,
+		    		"Content-Type": "application/json"
+		    	}
+			};
+
+			client.post(baseUrl+"/addtobill/v1/merchant/user/login",args,function(data, response){
+				if(data.error){
+					sails.log.debug('error in merchant user login');
+					res.negotiate({error:data.error, message: data.message});
+				}else{
+					sails.log.debug('successfull merchant user login');
+					res.json({paymentResponse:data});
 				}
-	    	},
-	    	headers:{
-	    		"Authorization" : req.headers.authorization,
-	    		"Content-Type": "application/json"
-	    	}
-		};
-
-		client.post(baseUrl+"/addtobill/v1/merchant/user/login",args,function(data, response){
-			if(data.error){
-				sails.log.debug('error in merchant user login');
-				res.negotiate({error:data.error, message: data.message});
-			}else{
-				sails.log.debug('successfull merchant user login');
-				res.json({paymentResponse:data});
-			}
-		});
+			});
+		}
 	}
 };
